a0e03ddedd2ae6d062e5474fc00f5718053c8cdf
Mathieu Eplenier <mathieu.eplenier@gmail.com>
2026-03-23 09:16:09 +0100

    Extraction du widget LoginForm à part

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
 
-
-
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 300),
+            child: LoginForm()
+          ),
+        )
+      )
+    );
+  }
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
 
   // Définition des Variables  
   final _trueUsername = "orlane";
@@ -24,52 +40,52 @@ class _LoginPageState extends State<LoginPage> {
   // Définitions des fonctions
   void _testLogin () {
 
-    if (_loginFormKey.currentState!.validate()) {
-      if (_usernameController.text == _trueUsername && _passwordController.text == _truePassword) {
+  //   if (_loginFormKey.currentState!.validate()) {
+  //     if (_usernameController.text == _trueUsername && _passwordController.text == _truePassword) {
+        
+  //     }
 
-      }
+  }
 
+  // Permet de libérer la mémoire si le formulaire est détruit
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 300),
-            child: Form(
-              key: _loginFormKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      label: Text("username"),
-                      border: OutlineInputBorder()
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      label: Text("password"),
-                      border: OutlineInputBorder()
-                    )
-                  ),
-                  SizedBox(height: 40),
-                  ElevatedButton(
-                    onPressed: _testLogin,
-                    child: Text("Valider")
-                  )
-                ]
-              )
+    return Form(
+      key: _loginFormKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: _usernameController,
+            decoration: const InputDecoration(
+              label: Text("username"),
+              border: OutlineInputBorder()
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(
+              label: Text("password"),
+              border: OutlineInputBorder()
             )
           ),
-        ),
-      ),
+          const SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: _testLogin,
+            child: const Text("Valider")
+          )
+        ]
+      )
     );
   }
 }
