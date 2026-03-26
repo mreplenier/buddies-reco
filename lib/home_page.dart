48a1586db0ec245228aa3f4df49acb82d2bba075
Thibault <kethibo12@gmail.com>
2026-03-26 22:20:06 +0100

    boutons d'ajout + setState de mise à jour

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,72 +13,50 @@ class _HomePageState extends State<HomePage> {
   // 1. LA MÉMOIRE : l'onglet actif
   int currentIndex = 0;
 
-  final List<String> entries = <String>[
-    'Lien 1',
-    'Lien 2',
-    'Lien 3',
-    'Lien 1',
-    'Lien 2',
-    'Lien 3',
-    'Lien 1',
-    'Lien 2',
-    'Lien 3',
-  ];
-  final List<int> colorCodes = <int>[
-    600,
-    500,
-    100,
-    600,
-    500,
-    100,
-    600,
-    500,
-    100,
-  ];
-
-  // 2. LA LISTE DES PAGES : ce qu'on affiche au milieu
-  late final List<Widget> pages = [
-    Center(
-      child: Column(
-        children: [
-          Text('🏠 Page Accueil'),
-          Expanded(
-            child: ListView.separated(
-              padding: const EdgeInsets.all(
-                30,
-              ), // pour ajouter des marges autour de la liste
-              itemCount: entries.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 120,
-                  color: Colors.amber[colorCodes[index]],
-                  child: Center(child: Text(entries[index])),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-            ),
-          ),
-        ],
-      ),
-    ),
-    const Center(child: Text('🔍 Page Recherche')),
-    const Center(child: Text('👤 Page Profil')),
-    const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('⚙️ Page Paramètres', textAlign: TextAlign.center),
-          Text(
-            'Mario c\'est pas un jeu, c\'est une licence ! Lequel, faut être précis !',
-          ),
-        ],
-      ),
-    ),
-  ];
+  final List<String> entries = <String>[];
+  final List<int> colorCodes = <int>[];
 
   @override
   Widget build(BuildContext context) {
+    late final List<Widget> pages = [
+      Center(
+        child: Column(
+          children: [
+            Text('🏠 Page Accueil'),
+            Expanded(
+              child: ListView.separated(
+                padding: const EdgeInsets.all(
+                  30,
+                ), // pour ajouter des marges autour de la liste
+                itemCount: entries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 120,
+                    color: Colors.amber[colorCodes[index]],
+                    child: Center(child: Text(entries[index])),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ),
+            ),
+          ],
+        ),
+      ),
+      const Center(child: Text('🔍 Page Recherche')),
+      const Center(child: Text('👤 Page Profil')),
+      const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('⚙️ Page Paramètres', textAlign: TextAlign.center),
+            Text(
+              'Mario c\'est pas un jeu, c\'est une licence ! Lequel, faut être précis !',
+            ),
+          ],
+        ),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(title: const Text('Buddies Reco'), centerTitle: true),
 
@@ -86,7 +64,12 @@ class _HomePageState extends State<HomePage> {
       body: pages[currentIndex],
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            entries.add("Nouveau lien spotify");
+            colorCodes.add(100);
+          });
+        },
         foregroundColor: Colors.white,
         backgroundColor: Colors.green,
         child: const Icon(Icons.add),
